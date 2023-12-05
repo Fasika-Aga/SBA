68359ce2bdcc65d4211b2e4768d1fc20c45c0769
Fasika Aga <fasikaaga7@gmail.com>
2023-12-05 00:08:00 -0500

    added Learner summition info and excute the array

--- index.js
+++ index.js
@@ -1,9 +1,9 @@
- const CourseInfo = {
+
+const CourseInfo = {
   id: 451,
   name: "Introduction to JavaScript",
 };
 
-// The provided assignment group.
 const AssignmentGroup = {
   id: 12345,
   name: "Fundamentals of JavaScript",
@@ -31,7 +31,6 @@ const AssignmentGroup = {
   ],
 };
 
-// The provided learner submission data.
 const LearnerSubmissions = [
   {
     learner_id: 125,
@@ -75,8 +74,6 @@ const LearnerSubmissions = [
   },
 ];
 
-function getLearnerData(course, ag, submissions) {
-// Function to calculate weighted average
 function calculateWeightedAverage(submissions, assignments) {
   let totalWeightedScore = 0;
   let totalWeight = 0;
@@ -84,28 +81,25 @@ function calculateWeightedAverage(submissions, assignments) {
   for (const submission of submissions) {
     const assignment = assignments.find((a) => a.id === submission.assignment_id);
 
-    // Check if assignment is not yet due
     if (new Date(assignment.due_at) > new Date()) {
       continue;
     }
 
-    // Deduct 10% for late submissions
     if (new Date(submission.submission.submitted_at) > new Date(assignment.due_at)) {
       submission.submission.score *= 0.9;
     }
 
-    totalWeightedScore += (submission.submission.score / assignment.points_possible) * assignment.points_possible * assignment.weight;
-    totalWeight += assignment.weight;
+    totalWeightedScore += (submission.submission.score / assignment.points_possible) * assignment.points_possible * assignment.group_weight;
+    totalWeight += assignment.group_weight;
   }
 
   if (totalWeight === 0) {
-    return 0; // Avoid division by zero
+    return 0;
   }
 
   return (totalWeightedScore / totalWeight) * 100;
 }
 
-// Function to process learner submissions
 function processLearnerSubmissions(assignmentGroup, learnerSubmissions) {
   const results = [];
 
@@ -121,12 +115,10 @@ function processLearnerSubmissions(assignmentGroup, learnerSubmissions) {
     for (const assignment of assignmentGroup.assignments) {
       const submission = learnerSubmissions.find((s) => s.assignment_id === assignment.id);
 
-      // Check if assignment is not yet due
       if (new Date(assignment.due_at) > new Date()) {
         continue;
       }
 
-      // Add percentage score for each assignment
       learnerResult[assignment.id] = (submission.submission.score / assignment.points_possible) * 100;
     }
 
@@ -136,48 +128,25 @@ function processLearnerSubmissions(assignmentGroup, learnerSubmissions) {
   return results;
 }
 
-// Main function
-function getLearnerData(courseInfo, assignmentGroup, learnerSubmissions) {
-  // Validate AssignmentGroup
-  validateAssignmentGroup(courseInfo, assignmentGroup);
-
-  // Process learnerSubmissions and calculate scores
-  const results = processLearnerSubmissions(assignmentGroup, learnerSubmissions);
-
-  return results;
-}
-
-// Validation function
 function validateAssignmentGroup(courseInfo, assignmentGroup) {
   if (courseInfo.id !== assignmentGroup.course_id) {
     throw new Error("AssignmentGroup does not belong to its course. Invalid input.");
   }
 }
 
+function getLearnerData(courseInfo, assignmentGroup, learnerSubmissions) {
+  validateAssignmentGroup(courseInfo, assignmentGroup);
+
+  const results = processLearnerSubmissions(assignmentGroup, learnerSubmissions);
+
+  return results;
+}
+
 // Example usage
 try {
-  const learnerData = getLearnerData(courseInfo, assignmentGroup, learnerSubmissions);
+  const learnerData = getLearnerData(CourseInfo, AssignmentGroup, LearnerSubmissions);
   console.log(learnerData);
 } catch (error) {
   console.error(error.message);
 }
-const result = [
-    {
-      id: 125,
-      avg: 0.985, // (47 + 150) / (50 + 150)
-      1: 0.94, // 47 / 50
-      2: 1.0, // 150 / 150
-    },
-    {
-      id: 132,
-      avg: 0.82, // (39 + 125) / (50 + 150)
-      1: 0.78, // 39 / 50
-      2: 0.833, // late: (140 - 15) / 150
-    },
-  ];
 
-  return result;
-}
-const result = getLearnerData(CourseInfo, AssignmentGroup, LearnerSubmissions);
-
-console.log(result);
